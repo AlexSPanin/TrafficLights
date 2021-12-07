3270da8547509c5d5424e92cb2d468f4dd6355f6
Panin <panin1970@gmail.com>
2021-12-07 23:49:17 +0300

    the logic of switching traffic lights has been worked out

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    var counterPressed = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.layer.cornerRadius = 40
+        redView.alpha = 0.3
+        yellowView.layer.cornerRadius = 40
+        yellowView.alpha = 0.3
+        greenView.layer.cornerRadius = 40
+        greenView.alpha = 0.3
+        startButton.setTitle("START", for: .normal)
     }
 
-
+    @IBAction func pressedButton() {
+            
+            startButton.setTitle("NEXT", for: .normal)
+            switch counterPressed {
+        case 0:
+            redView.alpha = 1
+                counterPressed += 1
+        case 1:
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+                counterPressed += 1
+        case 2:
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+                counterPressed += 1
+        case 3:
+            greenView.alpha = 0.3
+            startButton.setTitle("START", for: .normal)
+            counterPressed = 0
+            default:
+                return
+            }
+        }
 }
-
